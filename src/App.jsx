51ae6148d238e6d255fa36052bc775fea1dd6bff
gracetxgao <gracetxgao@gmail.com>
2023-08-13 14:31:22 -0700

    added comments

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import { Toaster } from 'react-hot-toast';
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
 
-
 function App() {
   const [tasks, setTasks] = useState([]); // declare list of all tasks
 
--- src/components/ListTasks.jsx
+++ src/components/ListTasks.jsx
@@ -4,12 +4,13 @@ import toast from "react-hot-toast"
 
 const ListTasks = ({tasks, setTasks}) => {
 
+    // declare all categories
     const [todos, setTodos] = useState([]);
     const [inProgress, setInProgress] = useState([]);
     const [closed, setClosed] = useState([]);
 
-
     useEffect (() => {
+        // categorize tasks
         const fTodos = tasks.filter((task) => task.status === "todo");
         const fInProgress = tasks.filter((task) => task.status === "inprogress");
         const fClosed = tasks.filter((task) => task.status === "closed");
@@ -24,6 +25,7 @@ const ListTasks = ({tasks, setTasks}) => {
 
     return (
         <div className="flex gap-16">
+            {/* duplicates the array statuses and maps inputed and new values in */}
             {statuses.map((status, index) => (
                 <Section 
                     key={index} 
@@ -41,15 +43,16 @@ const ListTasks = ({tasks, setTasks}) => {
 
 export default ListTasks;
 
-const Section = ({ status, tasks, setTasks, todos, inProgress, closed }) => {
-    const [{ isOver }, drop] = useDrop(() => ({
+const Section = ({status, tasks, setTasks, todos, inProgress, closed}) => {
+    const [{isOver}, drop] = useDrop(() => ({
         accept: "task",
         drop: (item) => addItemToSection(item.id),
         collect: (monitor) => ({
-            isOver: !!monitor.isOver(),
+            isOver: !!monitor.isOver()
         }),
     }));
     
+    // set up header variable styles
     let text = "Todo";
     let bg = "bg-slate-500";
     let tasksToMap = todos;
@@ -67,13 +70,17 @@ const Section = ({ status, tasks, setTasks, todos, inProgress, closed }) => {
     }
 
     const addItemToSection = (id) => {
-        setTasks(prev => {
+        // args: id of task to be added
+        // returns: list of all tasks in section
+        setTasks((prev) => {
+            // add new task to list of all tasks in section
             const mTasks = prev.map(t => {
-                if(t.id === id){
+                if(t.id === id){ // only for specific id of task to be moved
                     return {...t, status: status};
                 }
                 return t;
             });
+            // adds new list of section tasks to local storage in json format
             localStorage.setItem("tasks", JSON.stringify(mTasks));
             toast("Task status changed");
             return mTasks;
@@ -81,18 +88,27 @@ const Section = ({ status, tasks, setTasks, todos, inProgress, closed }) => {
     };
 
     return (
+        // create the section with information about tasks in section
         <div ref={drop} className={`w-64 rounded-md p-2 ${isOver ? "bg-slate-200" : ""}`}>
             <Header text={text} bg={bg} count={tasksToMap.length} /> 
-            {tasksToMap.length > 0 && tasksToMap.map(task => <Task key={task.id} task={task} tasks={tasks} setTasks={setTasks}/>)}
+            {tasksToMap.length > 0 && tasksToMap.map(task => 
+                <Task 
+                    key={task.id} 
+                    task={task} 
+                    tasks={tasks} 
+                    setTasks={setTasks}
+                />
+            )}
         </div>
     );
 };
 
 const Header = ({text, bg, count}) => {
+    // formatting the header... not sure why the variables can't be set up right before this part
     return (
         <div className={`${bg} flex items-center h-12 pl-4 rounded-md uppercase text-sm text-white`}>
            {text}
-            <div className="ml-2 bg-white w-5 h-5 text-black rounded-full flex items-center justify-center">
+            <div className="ml-2 bg-white w-5 h-5 flex items-center text-black rounded-full justify-center">
                 {count}
             </div>
         </div>
@@ -100,16 +116,17 @@ const Header = ({text, bg, count}) => {
 };
 
 const Task = ({task, tasks, setTasks}) => {
-
+    // sets up dragging again?
     const [{ isDragging }, drag] = useDrag(() => ({
         type: "task",
         item: {id: task.id},
         collect: (monitor) => ({
             isDragging: !!monitor.isDragging()
-        }),
+        })
     }));
 
     const handleRemove = (id) => {
+        // filter out task to be removed from list of all tasks (in section?)
         const ftasks = tasks.filter(t => t.id !== id);
 
         localStorage.setItem("tasks", JSON.stringify(ftasks));
@@ -117,13 +134,17 @@ const Task = ({task, tasks, setTasks}) => {
 
         toast("Task removed");
     }
+    
     return (
         <div 
             ref = {drag} 
             className={`relative pt-4 mt-8 shadow-md rounded-md cursor-grab ${isDragging ? "opacity-25" : "opacity-100"}`}
         >
             <p>{task.name}</p>
-            <button className="absolute bottom-1 right-1 text-slate-400" onClick={() => handleRemove(task.id)}>
+            <button 
+                className="absolute bottom-1 right-1 text-slate-400" 
+                onClick={() => handleRemove(task.id)}
+            >
                 <svg 
                     xmlns="http://www.w3.org/2000/svg" 
                     fill="none" 
