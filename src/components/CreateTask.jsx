23d98665ea62d905933990ec746ed3951ddbd16a
gracetxgao <gracetxgao@gmail.com>
2023-08-13 13:31:43 -0700

    added comments

--- src/components/CreateTask.jsx
+++ src/components/CreateTask.jsx
@@ -2,27 +2,27 @@ import { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
 import toast from "react-hot-toast"
 
-
-
-
-
 const CreateTask = ({tasks, setTasks}) => {
-        const [task, setTask] = useState({
-            id: "",
-            name: "",
-            status: "todo", // or in progess or finished
-        });
+    const [task, setTask] = useState({ // sets default empty task
+        id: "",
+        name: "",
+        status: "todo", // or in progess or finished
+    });
 
     const handleSubmit = (e) => {
-        e.preventDefault();
+        e.preventDefault(); // stops default action (reload) on render
 
         if (task.name.length < 3) return toast.error("Task must be more than three characters");
         if (task.name.length > 100) return toast.error("Task must be less than 100 characters");
 
         setTasks((prev) => {
+            // args: list of previously added tasks
+            // adds new task to list of all tasks
+            // returns: list of all tasks updated
+
             const list = [...prev, task];
 
-            localStorage.setItem("tasks", JSON.stringify(list));
+            localStorage.setItem("tasks", JSON.stringify(list)); // adds key and item to local storage
 
             return list;
         });
@@ -30,6 +30,7 @@ const CreateTask = ({tasks, setTasks}) => {
         toast.success("Task Created")
 
         setTask({
+            // template for new task
             id: "",
             name: "",
             status: "todo", // or in progess or finished
@@ -37,9 +38,11 @@ const CreateTask = ({tasks, setTasks}) => {
     };
 
     return <form onSubmit={handleSubmit}>
-        <input type='text' className="border-2 border-slate-400 bg-slate-100 rounded-md mr-4 h-12 w-64 px-1"
-        value={task.name}
-        onChange={(e)=> setTask({...task, id: uuidv4(), name: e.target.value})}
+        <input 
+            type='text' 
+            className="border-2 border-slate-400 bg-slate-100 rounded-md mr-4 h-12 w-64 px-1"
+            value={task.name}
+            onChange={(e)=> setTask({...task, id: uuidv4(), name: e.target.value})} // creates new task
         />
         <button className="bg-cyan-500 rounded-md px-4 h-12 text-white">Create</button>
     </form>;
