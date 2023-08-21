4a1f26f7db52f2545341158e4b0cd3ac1c16da0f
gracetxgao <gracetxgao@gmail.com>
2023-08-20 22:42:50 -0700

    began user authentication

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App.jsx'
 import './index.css'
+import LoginButton from './Login.jsx';
+import LogoutButton from './Logout.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const root = createRoot(document.getElementById('root'));
+
+root.render(
+  <Auth0Provider
+    domain="dev-fgvtmeuortr1ndkv.us.auth0.com"
+    clientId="nw9VRD21qp2zbn6CqcLE30zU1MJG98ew"
+    authorizationParams={{
+      redirect_uri: "http://localhost:5173"
+    }}
+  >
+    <LoginButton>Log In</LoginButton>
+    <LogoutButton>Log Out</LogoutButton>
     <App />
-  </React.StrictMode>,
-)
+  </Auth0Provider>,
+);
