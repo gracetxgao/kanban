bd720925bc138adbedfc42549ea0124dda2b0cda
gracetxgao <gracetxgao@gmail.com>
2023-08-22 11:02:31 -0700

    fixed authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,19 @@
-import { useEffect, useState } from 'react';
-import CreateTask from './components/CreateTask';
-import ListTasks from './components/ListTasks';
-import { Toaster } from 'react-hot-toast';
-import { DndProvider } from 'react-dnd'
-import { HTML5Backend } from 'react-dnd-html5-backend'
+import LoginButton from './Login';
+import Kanban from './Kanban';
+import Profile from './Profile';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
-  const [tasks, setTasks] = useState([]); // declare list of all tasks
-
-  console.log("tasks", tasks);
-
-  useEffect(() => {
-    setTasks(JSON.parse(localStorage.getItem("tasks")));
-  }, []); // calls all previously stored tasks
-
   return (
-    <DndProvider backend={HTML5Backend}>
-      <Toaster/>
-      <div className='bg-slate-100 w-screen h-screen flex flex-col items-center p-3 gap-16 pt-32'>
-        <CreateTask tasks={tasks} setTasks={setTasks}/>
-        <ListTasks tasks={tasks} setTasks={setTasks}/>
-      </div>
-    </DndProvider>
+    <div className='App'>
+      <Routes>
+        <Route path='/' element={ <LoginButton /> } />
+        <Route path='/kanban' element={ <Kanban />} />
+        <Route path='/profile' element={ <Profile />} />
+      </Routes>      
+    </div>
+
   );
-}
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
 import { createRoot } from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App.jsx'
 import './index.css'
-import LoginButton from './Login.jsx';
-import LogoutButton from './Logout.jsx';
+import { BrowserRouter } from 'react-router-dom';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-  <Auth0Provider
-    domain="dev-fgvtmeuortr1ndkv.us.auth0.com"
-    clientId="nw9VRD21qp2zbn6CqcLE30zU1MJG98ew"
-    authorizationParams={{
-      redirect_uri: "http://localhost:5173"
-    }}
-  >
-    <LoginButton>Log In</LoginButton>
-    <LogoutButton>Log Out</LogoutButton>
-    <App />
-  </Auth0Provider>,
+  <BrowserRouter>
+    <Auth0Provider
+      domain="dev-fgvtmeuortr1ndkv.us.auth0.com"
+      clientId="nw9VRD21qp2zbn6CqcLE30zU1MJG98ew"
+      authorizationParams={{
+        redirect_uri: "http://localhost:5173"
+      }}
+    >
+      <App />
+    </Auth0Provider>  
+  </BrowserRouter>
+
 );
